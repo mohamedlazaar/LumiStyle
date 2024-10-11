7dfab6fafbd0f00949db0b50db687684ea38beff
mohamed lazaar <lazzarmohamed10@gmail.com>
2024-10-11 11:37:20 +0100

    changing some data

--- src/NosProduits.jsx
+++ src/NosProduits.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import AllProducts from './components/sections/AllProducts'
-import {featuredProductData} from './data.js'
+import {collectionData} from './collectionData'
 
 function NosProduits() {
   return (
     <div className='container mx-auto flex flex-col gap-5 pt-24 '>
         <h1 className='text-5xl font-extrabold '>Nos Produits</h1>
-        <AllProducts data={featuredProductData} /> 
+        <AllProducts data={collectionData} /> 
     </div>
   )
 }
--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { collectionData } from './collectionData.js';
 import ProductDetails from './components/sections/productDetails.jsx';
 import FeaturedProduct from './components/sections/FeaturedProduct';
-import {BannerProductData} from './data.js'
+import {featuredProductData} from './data.js'
 
 
 function ProductPage() {
   return (
     <>
     <ProductDetails  data={collectionData}/>
-    <FeaturedProduct data={BannerProductData} />
+    <FeaturedProduct data={featuredProductData} />
     </>
   )
 }
