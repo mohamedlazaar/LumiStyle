b3ead64fa2b49009cbaa502e5c83dba117f9d47f
mohamed lazaar <lazzarmohamed10@gmail.com>
2024-10-13 00:33:33 +0100

    adding svg to the share buttons

--- src/components/sections/productDetails.jsx
+++ src/components/sections/productDetails.jsx
@@ -130,7 +130,9 @@ function ProductDetails({ data }) {
                     rel="noopener noreferrer"
                     className="hover:text-blue-600"
                     aria-label="Share on Facebook">
-                    {/* SVG Icon */}
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-6 h-6">
+                        <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.437 9.879v-6.987h-2.54v-2.71h2.54v-2.071c0-2.507 1.492-3.89 3.778-3.89 1.096 0 2.238.195 2.238.195v2.465h-1.26c-1.244 0-1.63.772-1.63 1.562v1.839h2.773l-.443 2.71h-2.33v6.987C18.343 21.128 22 16.991 22 12z" />
+                      </svg>
                   </a>
 
                   {/* Twitter Share Button */}
@@ -139,8 +141,21 @@ function ProductDetails({ data }) {
                     rel="noopener noreferrer"
                     className="hover:text-blue-400"
                     aria-label="Share on Twitter">
-                    {/* SVG Icon */}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-6 h-6">
+                      <path d="M23.643 4.937c-.835.37-1.73.62-2.675.732a4.673 4.673 0 0 0 2.048-2.578 9.222 9.222 0 0 1-2.916 1.113 4.66 4.66 0 0 0-7.946 4.247 13.223 13.223 0 0 1-9.595-4.865 4.634 4.634 0 0 0-.63 2.345 4.658 4.658 0 0 0 2.071 3.878 4.606 4.606 0 0 1-2.113-.584v.06a4.656 4.656 0 0 0 3.733 4.565 4.684 4.684 0 0 1-2.105.08 4.662 4.662 0 0 0 4.348 3.234A9.352 9.352 0 0 1 0 19.539 13.164 13.164 0 0 0 7.162 21c8.804 0 13.622-7.294 13.622-13.622 0-.208-.004-.416-.014-.623a9.733 9.733 0 0 0 2.373-2.474l-.5-.344z" />
+                    </svg>
+
                   </a>
+                  {/* WhatsApp Share Button */}
+                    <a href={`https://wa.me/?text=${encodeURIComponent(`Check out this amazing product: ${product.title} - only ${product.price}! ${website_url}/product/${id} \n${product.featured_image}`)}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="hover:text-green-500"
+                      aria-label="Share on WhatsApp">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-6 h-6">
+                          <path d="M12 0C5.373 0 0 5.373 0 12c0 2.138.559 4.221 1.624 6.065l-1.07 3.89 4.01-1.046A11.954 11.954 0 0 0 12 24c6.627 0 12-5.373 12-12S18.627 0 12 0zm5.833 17.053a1.05 1.05 0 0 1-1.006.572c-2.047-.095-3.665-1.113-5.091-2.617-1.426-1.504-2.42-3.23-2.842-5.164a1.062 1.062 0 0 1 .288-1.037 1.092 1.092 0 0 1 1.074-.258c.475.109.983.365 1.52.771a12.568 12.568 0 0 0 1.676 1.065c.49.21.796.376 1.21.502a1.034 1.034 0 0 1 .619.625 1.1 1.1 0 0 1-.197 1.078l-.416.41a.242.242 0 0 0-.026.027c.005.027.018.054.039.082.062.083.146.159.255.229.198.13.476.283.79.446.568.283.86.35 1.172.3a.545.545 0 0 0 .457-.492.53.53 0 0 0-.29-.5z" />
+                        </svg>
+                    </a>
 
                   {/* LinkedIn Share Button */}
                   <a href={`https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(`${website_url}/product/${id}`)}&image=${encodeURIComponent(product.featured_image)}`}
@@ -148,7 +163,9 @@ function ProductDetails({ data }) {
                     rel="noopener noreferrer"
                     className="hover:text-blue-700"
                     aria-label="Share on LinkedIn">
-                    {/* SVG Icon */}
+                   <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-6 h-6">
+                      <path d="M19.994 3H4.007A1.007 1.007 0 0 0 3 4.006v15.988C3 20.551 3.448 21 4.007 21h15.987C20.552 21 21 20.552 21 19.994V4.007A1.007 1.007 0 0 0 19.994 3zM8.442 18.56H5.876V9.559h2.566v9.001zm-1.283-10.26a1.478 1.478 0 1 1 0-2.956 1.478 1.478 0 0 1 0 2.956zm11.399 10.26h-2.566v-4.73c0-1.128-.02-2.584-1.577-2.584-1.577 0-1.818 1.235-1.818 2.505v4.808h-2.566V9.559h2.465v1.226h.035c.343-.652 1.182-1.338 2.43-1.338 2.598 0 3.079 1.71 3.079 3.933v5.18z" />
+                    </svg>
                   </a>
 
                   {/* Pinterest Share Button */}
@@ -157,7 +174,9 @@ function ProductDetails({ data }) {
                     rel="noopener noreferrer"
                     className="hover:text-red-600"
                     aria-label="Share on Pinterest">
-                    {/* SVG Icon */}
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-6 h-6">
+                        <path d="M12 0C5.372 0 0 5.372 0 12c0 4.847 3.057 8.952 7.365 10.406-.102-.886-.194-2.248.041-3.216.211-.875 1.356-5.575 1.356-5.575s-.347-.694-.347-1.717c0-1.606.931-2.808 2.09-2.808.986 0 1.462.739 1.462 1.622 0 .989-.63 2.464-.953 3.835-.272 1.156.578 2.1 1.717 2.1 2.064 0 3.654-2.173 3.654-5.304 0-2.775-1.995-4.717-4.851-4.717-3.305 0-5.231 2.472-5.231 5.027 0 .991.382 2.056.859 2.634.094.114.107.214.079.33-.086.353-.28 1.123-.318 1.279-.05.214-.163.258-.38.157-1.418-.622-2.3-2.576-2.3-4.158 0-3.379 2.455-6.49 7.086-6.49 3.712 0 6.598 2.646 6.598 6.167 0 3.691-2.32 6.663-5.553 6.663-1.083 0-2.102-.562-2.45-1.227l-.664 2.526c-.238.911-.885 2.054-1.32 2.748 1.001.31 2.056.479 3.154.479 6.627 0 12-5.373 12-12S18.627 0 12 0z"/>
+                      </svg>
                   </a>
 
                 </div>
